0d0dabbce44781c5ae3159e763a08751a6b23fca
jessgriffin <jessica.griffin@ocsbstudent.ca>
2016-12-01 14:48:49 -0500

    Create Square.java

---
+++ Square.java
@@ -0,0 +1,21 @@
+
+public class Square {
+	
+	//properties
+	//private int _position;
+	private Color _color;
+	
+	//constructor
+	public Square(int position, Color color) {
+		//this._position = position;
+		this._color = color;
+	}
+	
+	//getter
+	//public int getPosition(){ return this._position; }
+	public Color getColor(){ return this._color; }
+	
+	//setter
+	//public void setPosition(int pos){ this._position = pos; }
+
+}
